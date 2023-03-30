9ce35862c7338983c09ba004ef96b508925cb19d
Lucas <lucas_lirac@outlook.com>
2023-03-30 15:13:08 -0300

    components

---
+++ src/components/header/index.tsx
@@ -0,0 +1,21 @@
+
+
+function Header() {
+    return (
+        <div className="Hero">
+            <nav>
+                <h2 className="Logo">Ecommerce</h2>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Service</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <button type="button">Sigin</button>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
