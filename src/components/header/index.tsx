1ed0f30d74fae75f91ac1e100759a901523985d3
Lucas <lucas_lirac@outlook.com>
2023-03-30 19:39:45 -0300

    nao lembro

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,22 @@
 
+import { useNavigate } from 'react-router-dom';
 import './style.css'
 
 
 function Header() {
+    const navigate = useNavigate();
     return (
         <div className="Hero">
             <nav>
                 <h2 className="Logo">Ecommerce</h2>
                 <ul>
-                    <li><a href="#">Home</a></li>
+                    <li><a href="" onClick={() => navigate("/")}>Home</a></li>
                     <li><a href="#">Products</a></li>
                     <li><a href="#">Service</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
-                <button type="button">Sign In</button>
+                <button type="button" className='header-button' onClick={() => navigate("/Login")}>Sign In</button>
             </nav>
         </div>
     )
