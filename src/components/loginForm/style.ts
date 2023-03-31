ff7a5e53e0c2357eeca141fe7fe8bdd91baaf821
Lucas <lucas_lirac@outlook.com>
2023-03-31 12:57:44 -0300

    modificando style

--- src/components/loginForm/style.ts
+++ src/components/loginForm/style.ts
@@ -17,12 +17,12 @@ html {
 
 .container-login {
   width: 100%;
-  min-height: 100vh;
+
   display: flex;
-  flex-wrap: wrap;
+  
   align-items: center;
   justify-content: center;
-  padding: 15px;
+  padding: 100px;
   background-color: #111;
 }
 
